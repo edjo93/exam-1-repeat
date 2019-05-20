a75b08a380d5c82da944cceac806ff9c89a28238
edjo93 <eddas.carrasco@unitec.edu>
2019-05-20 08:10:45 -0600

    funcionalidad casi terminada / faltan detalles de orden en main

--- examen1.cpp
+++ examen1.cpp
@@ -12,6 +12,10 @@ void imprimir_matriz(char**,int);
 void liberar_matriz(char**,int);
 void crear_tablero(char**,int );
 void mover(char**matriz,int xi,int yi,int xf,int yf,char pieza);
+bool gano_rey(char**matriz,int size);
+bool ganan_moskovitas(char**matriz,int size);
+void captura_guardianes(char**,int);
+void colocar_x(char**matriz,int size);
 //menu repetitivo
 int main(){
     char opcion;
@@ -19,6 +23,9 @@ int main(){
     int y;
     int size=11;
     char** matriz=NULL;
+    char sueco;//leemos si se movera el rey o una pieza comun
+    int xf;
+    int yf;
     do{
     
     cout<<"\nseleccione una opcion\n\n1.op1\ns.salir\n?:";
@@ -32,31 +39,89 @@ int main(){
             //creamos el tablero
             crear_tablero(matriz,size);
             //mostramos el tablero
-            imprimir_matriz(matriz,size);
-            cout<<"ingrese x:";
-            cin>>x;
-            cout<<"ingrese y:";
-            cin>>y;
+            cout<<"tablero listo";
             
-            //primer turno
-            while(!tomar_correcta(matriz,x,y,'n')){//mientras no tomemos una pieza correcta para el turno
-                //continuamos pidiendo que se tome una pieza correcta
-                cout<<"\nno se ha tomado una pieza correta intente de nuevo\n";
-                cout<<"ingrese x:";
+            
+            
+            while(!gano_rey(matriz,size)&&!ganan_moskovitas(matriz,size)){//mientras no gane el rey ni tampoco los moskovitas el juego continua
+                cout<<"\njuegan moskovitas"<<endl;
+                //primer turno
+                imprimir_matriz(matriz,size);
+                cout<<"ingrese xi:";
                 cin>>x;
-                cout<<"ingrese y:";
+                cout<<"ingrese yi:";
                 cin>>y;
-                tomar_correcta(matriz,x,y,'n');
+                while(!tomar_correcta(matriz,x,y,'n')){//mientras no tomemos una pieza correcta para el turno
+                    //continuamos pidiendo que se tome una pieza correcta
+                    cout<<"\nno se ha tomado una pieza correcta intente de nuevo\n";
+                    imprimir_matriz(matriz,size);
+                    cout<<"ingrese x:";
+                    cin>>x;
+                    cout<<"ingrese y:";
+                    cin>>y;
+                    tomar_correcta(matriz,x,y,'n');
+                }
+                cout<<"ingrese xf:";
+                cin>>xf;
+                cout<<"ingrese yf:";
+                cin>>yf;
+                
+                //los moskovitas se mueven
+                mover(matriz,x,y,xf,yf,'n');
+                imprimir_matriz(matriz,size);
+                //se verifica si el rey gano
+                captura_guardianes(matriz,size);
+                colocar_x(matriz,size);
+                gano_rey(matriz,size);
+                ganan_moskovitas(matriz,size);
+                
+                
+
+                if(!gano_rey(matriz,size)&&!ganan_moskovitas(matriz,size)){//si el rey no ha ganado la siguiente jugada continua y tampoco los moskovitas
+                    cout<<"\nturno de los suecos\nw-mover rey\nb-pieza comun\n?:";
+                    cin>>sueco;
+                    while(sueco!='w'&&sueco!='b'){//mientras no se elija correctamente una pieza comun o un rey continuamos pidiendo la pieza
+                        cout<<"\nturno de los suecos\nr-mover rey\nc-pieza comun\n?:";
+                        cin>>sueco;
+
+                    }
+                    cout<<"ingrese x:";
+                    cin>>x;
+                    cout<<"ingrese y:";
+                    cin>>y;
+                    //segundo turno
+                    while(!tomar_correcta(matriz,x,y,sueco)){//mientras no tomemos una pieza correcta para el turno
+                        //continuamos pidiendo que se tome una pieza correcta
+                        cout<<"\nno se ha tomado una pieza correcta intente de nuevo\n";
+                        imprimir_matriz(matriz,size);
+                        cout<<"ingrese x:";
+                        cin>>x;
+                        cout<<"ingrese y:";
+                        cin>>y;
+                        tomar_correcta(matriz,x,y,sueco);
+                    }
+                    imprimir_matriz(matriz,size);
+                    
+                    cout<<"ingrese xf:";
+                    cin>>xf;
+                    cout<<"ingrese yf:";
+                    cin>>yf;
+                    //los moskovitas se mueven
+                    mover(matriz,x,y,xf,yf,sueco);
+                    imprimir_matriz(matriz,size);
+                    captura_guardianes(matriz,size);
+                    colocar_x(matriz,size);
+                    gano_rey(matriz,size);
+                    ganan_moskovitas(matriz,size);
+
+                }
+                
+
             }
-            imprimir_matriz(matriz,size);
-            int xf;
-            int yf;
-            cout<<"ingrese xf:";
-            cin>>xf;
-            cout<<"ingrese yf:";
-            cin>>yf;
-            mover(matriz,x,y,xf,yf,'n');
-            imprimir_matriz(matriz,size);
+
+
+            
+
 
             //liberamos la matriz
             liberar_matriz(matriz,size);
@@ -230,17 +295,30 @@ void mover(char**matriz,int xi,int yi,int xf,int yf,char pieza){
         
         if (xi==xf){//el movimiento sera en la fila
             //validamos que no se pueda saltar piezas
-            for(int i=yi+1;i<=yf;i++){
-                if(matriz[xi][i]!=' '){
-                    haypieza=true;
-                }
+            if(yi<yf){
+                for(int i=yi+1;i<=yf;i++){
+                    if(matriz[xi][i]!=' '&&matriz[xi][i]!='x'){
+                        haypieza=true;
+                    }
 
+                }
+            }else{
+                for(int i=yi-1;i>=yf;i--){
+                    if(matriz[xi][i]!=' '&&matriz[xi][i]!='x'){
+                        haypieza=true;
+                    }
+
+                }
             }
+            
+
             if(!haypieza){//el movimiento se puede hacer si no hay pieza en el camino
                 //la casilla destino es x
                 if(matriz[xf][yf]=='x'){
                     es_x=true;
                 }
+
+
 
                 if(!es_x){//no es x la casilla destino
                     matriz[xi][yi]=' ';//la pieza se movio de ese sitio
@@ -260,12 +338,23 @@ void mover(char**matriz,int xi,int yi,int xf,int yf,char pieza){
         
         }else{
             if (yi==yf){//el movimiento sera en la columna
-                for(int i=xi+1;i<=xf;i++){
-                    if(matriz[i][yi]!=' '){
-                        haypieza=true;
-                    }
+                if(xi<xf){
+                    for(int i=xi+1;i<=xf;i++){
+                        if(matriz[i][yi]!=' '&&matriz[i][yi]!='x'){
+                            haypieza=true;
+                        }
 
+                    }
+                }else{
+                    for(int i=xi-1;i>=xf;i--){
+                        if(matriz[i][yi]!=' '&&matriz[i][yi]!='x'){
+                            haypieza=true;
+                        }
+
+                    }
                 }
+                
+                
                 if(!haypieza){//el movimiento se puede hacer si no hay pieza en el camino
                     //la casilla destino es x
                     if(matriz[xf][yf]=='x'){
@@ -291,15 +380,26 @@ void mover(char**matriz,int xi,int yi,int xf,int yf,char pieza){
 
         }
 
-    }else{//mismo movimiento solo que con una restriccion
+    }else{//mismo movimiento con diferente restriccion
         if (xi==xf){//el movimiento sera en la fila
             //validamos que no se pueda saltar piezas
-            for(int i=yi+1;i<=yf;i++){
-                if(matriz[xi][i]!=' '){
-                    haypieza=true;
-                }
+            if(yi<yf){
+                for(int i=yi+1;i<=yf;i++){
+                    if(matriz[xi][i]!=' '&&matriz[xi][i]!='x'){
+                        haypieza=true;
+                    }
 
+                }
+            }else{
+                for(int i=yi-1;i>=yf;i--){
+                    if(matriz[xi][i]!=' '&&matriz[xi][i]!='x'){
+                        haypieza=true;
+                    }
+
+                }
             }
+
+
             if(!haypieza){//el movimiento se puede hacer si no hay pieza en el camino
                 matriz[xi][yi]=' ';//la pieza se movio de ese sitio
                 matriz[xf][yf]=pieza;//la pieza se movio a este sitio
@@ -311,12 +411,23 @@ void mover(char**matriz,int xi,int yi,int xf,int yf,char pieza){
         
         }else{
             if (yi==yf){//el movimiento sera en la columna
-                for(int i=xi+1;i<=xf;i++){
-                    if(matriz[i][yi]!=' '){
-                        haypieza=true;
-                    }
+                if(xi<xf){
+                    for(int i=xi+1;i<=xf;i++){
+                        if(matriz[i][yi]!=' '&&matriz[i][yi]!='x'){
+                            haypieza=true;
+                        }
 
+                    }
+                }else{
+                    for(int i=xi-1;i>=xf;i--){
+                        if(matriz[i][yi]!=' '&&matriz[i][yi]!='x'){
+                            haypieza=true;
+                        }
+
+                    }
                 }
+
+
                 if(!haypieza){//el movimiento se puede hacer si no hay pieza en el camino
                     matriz[xi][yi]=' ';//la pieza se movio de ese sitio
                     matriz[xf][yf]=pieza;//la pieza se movio a este sitio    
@@ -336,5 +447,132 @@ void mover(char**matriz,int xi,int yi,int xf,int yf,char pieza){
     
     
 
+
+}
+
+bool gano_rey(char**matriz,int size){
+    bool gano=false;//el rey no es ganador
+    //el rey gana cuando llega a una x
+    for(int x=0;x<size;x++){
+        for(int y=0;y<size;y++){
+            //se revisan todas las esquinas
+            if(matriz[0][0]=='w'){
+                gano=true;
+
+            }
+            if(matriz[0][size-1]=='w'){
+                gano=true;
+                
+            }
+            if(matriz[size-1][0]=='w'){
+                gano=true;
+                
+            }
+            if(matriz[size-1][size-1]=='w'){
+                gano=true;
+                
+            }
+        }
+    }
+    return gano;
+
+}
+
+void captura_guardianes(char**matriz,int size){
+    for(int x=0;x<size;x++){
+        for(int y=0;y<size;y++){
+            if(matriz[x][y]=='n'){//verificamos que no hayan rodeado a un moscovita
+                //evitamos salir de la matriz
+
+                //casilla valida(columna)
+                if(x-1>=0&&x+1<=size-1){
+                    if(matriz[x-1][y]=='b'&&matriz[x+1][y]=='b'){
+                        matriz[x][y]=' ';
+                    }
+                    if(matriz[x-1][y]=='w'&&matriz[x+1][y]=='b'){
+                        matriz[x][y]=' ';
+                    }
+                    if(matriz[x-1][y]=='b'&&matriz[x+1][y]=='w'){
+                        matriz[x][y]=' ';
+                    }
+                }
+
+                //casilla valiada(fila)
+                if(y+1<=size-1&&y-1>=0){
+                    if(matriz[x][y-1]=='b'&&matriz[x][y+1]=='b'){
+                        matriz[x][y]=' ';
+                    }
+                    if(matriz[x][y-1]=='w'&&matriz[x][y+1]=='b'){
+                        matriz[x][y]=' ';
+                    }
+                    if(matriz[x][y-1]=='b'&&matriz[x][y+1]=='w'){
+                        matriz[x][y]=' ';
+                    }   
+                }
+
+            }else{//se verifica que no hayan rodeado a un sueco(exceptuando al rey)
+                if(matriz[x][y]!='w'){
+                    //evitamos salir de la matriz
+
+                    //casilla valida(columna)
+                    if(x-1>=0&&x+1<=size-1){
+                        if(matriz[x-1][y]=='n'&&matriz[x+1][y]=='n'){
+                            matriz[x][y]=' ';
+                        }
+                        
+                    }
+
+                    //casilla valiada(fila)
+                    if(y+1<=size-1&&y-1>=0){
+                        if(matriz[x][y-1]=='n'&&matriz[x][y+1]=='n'){
+                            matriz[x][y]=' ';
+                        }
+                        
+                    }    
+                }
+                
+                   
+            }
+        }
+    }   
+
+}
+
+bool ganan_moskovitas(char**matriz,int size){
+    
+    bool gano=false;
+    //los suecos ganan cuando han rodeado al rey
+    for(int x=0;x<size;x++){
+        for(int y=0;y<size;y++){
+            //validamos que el rey no este rodeado
+
+            //buscamos el rey
+            if(matriz[x][y]=='w'){
+                //buscamos a su alrededor
+
+                //si todas las casillas son validas
+                if(x-1>=0&&x+1<=size-1&&y+1<=size-1&&y-1>=0){
+                    if(matriz[x-1][y]=='n'&&matriz[x+1][y]=='n'&&matriz[x][y-1]=='n'&&matriz[x][y+1]=='n'){
+                        gano=true;//los suecos ganan
+                    }
+                }
+            }
+
+
+
+        }
+    }
+
+    return gano;
+}
+
+void colocar_x(char**matriz,int size){//funcion que coloca una X en el tablero cuando el rey no esta en esa posicion
+    for(int x=0;x<size;x++){
+        for(int y=0;y<size;y++){
+            if(matriz[size/2][size/2]!='w'){
+                matriz[size/2][size/2]='x';
+            }
+        }
+    }
 
 }
